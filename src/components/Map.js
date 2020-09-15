ddd61e24528dc711fef267dea5bfc1735ffa7bf6
Sam Johnston <johnstonsam712@gmail.com>
2020-09-15 12:34:52 +0100

    Adding popup on marker pressed

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import ReactMapGl, {Marker} from 'react-map-gl';
+import ReactMapGl, {Marker, Popup} from 'react-map-gl';
 import axios from 'axios';
 
 export default function Map() {
@@ -12,6 +12,7 @@ export default function Map() {
   })
   const [loading, setLoading] = useState(true);
   const [policeData, setPoliceData] = useState([])
+  const [selectedCrime, setSelectedCrime] = useState(null)
 
   useEffect(() => {
     setLoading(true)
@@ -36,19 +37,39 @@ export default function Map() {
     {...viewport}
     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     onViewportChange={(viewport => setViewport(viewport))}
+    mapStyle="mapbox://styles/sjohnston00/ckf3u6kt90x0j1and9dg4n7cf"
     >
 
-    {policeData.map((crime, i) => (
+    {policeData.map((crime) => (
       <Marker
         key={crime.id}
         latitude={Number(crime.location.latitude)}
         longitude={Number(crime.location.longitude)}
       >
-      <button className="icon-button">
+      <button className="icon-button" onClick={e => {
+        e.preventDefault()
+        setSelectedCrime(crime);
+      }}>
         <img className="icon" src="/marker.svg" alt="Crime icon"/>
       </button>
       </Marker>
     ))}
+
+    {selectedCrime && (
+      <Popup
+        latitude={Number(selectedCrime.location.latitude)}
+        longitude={Number(selectedCrime.location.longitude)} 
+        className="popup"
+        onClose={() => setSelectedCrime(null)}
+      >
+        <div>
+          <h2>{selectedCrime.category}</h2>
+          <p>Location: {selectedCrime.location.street.name}</p>
+          {selectedCrime.outcome_status && <p>Outcome: {selectedCrime.outcome_status.category}</p>}
+          <i>Date: {selectedCrime.month}</i>
+        </div>
+      </Popup>
+      )}
     <div className="config-area">
       <button className="refresh-button" disabled={loading} onClick={refresh}>{loading ? 'Loading...' : 'Refresh'}</button>
       <div className="input-group">
@@ -58,12 +79,28 @@ export default function Map() {
       <div className="input-group">
         <label htmlFor="Crime">Crime</label>
         <select>
-          <option value="All-Crime">All Crime</option>
-          <option value="Bicycle-Theft">Bicycle-Theft</option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
+          <option value="all-crime">All Crime</option>
+          <option value="anti-social-behaviour">Anti Social Behaviour</option>
+          <option value="bicycle-theft">Bicycle Theft</option>
+          <option value="burglary">Burglary</option>
+          <option value="criminal-damage-arson">Criminal damage and arson</option>
+          <option value="drugs">Drugs</option>
+          <option value="other-theft">Other Theft</option>
+          <option value="possession-of-weapons">Possession Of Weapons</option>
+          <option value="public-order">Public Order</option>
+          <option value="robbery">Robbery</option>
+          <option value="shoplifting">Shoplifting</option>
+          <option value="theft-from-the-person">Theft From The Person</option>
+          <option value="vehicle-crime">Vehicle Crime</option>
+          <option value="violent-crime">Violent Crime</option>
+          <option value="other-crime">Other Crime</option>
         </select>
+      </div>
+
+      <div>
+        <p>Number of crimes: {policeData.length}</p>
+        <p>Longitude: {viewport.longitude.toFixed(4)}</p>
+        <p>Latitude: {viewport.latitude.toFixed(4)}</p>
       </div>
 
     </div>
