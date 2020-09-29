cd070f10ac459947d64c707e233827f89492d10e
Sam Jonston <johnstonsam712@gmail.com>
2020-09-29 15:56:31 +0100

    Adding info section and getting started on Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Map from './components/Map'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import Info from "./components/Middle Section/Info/Info";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Info/>
     <h1 style={{textAlign: 'center', marginTop: '30px', fontSize: '50px'}}>Our Map</h1>
     <Map></Map>
+    <Footer/>
     </>
   );
 }
