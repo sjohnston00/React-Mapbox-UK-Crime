e2287760086c171bb7d2a70e38b1f1ffbf50422a
Sam Jonston <johnstonsam712@gmail.com>
2020-10-07 16:06:34 +0100

    Adding a new markers component that caches the marker component for each crime, and allows it to not rerender on viewport change for a bigger performance improvement when more markers are on the map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,7 +2,8 @@
 import React, {useState, useEffect} from 'react';
 import MapButton from './MapButton/MapButton';
 import Sidebar from './Sidebar/Sidebar';
-import ReactMapGl, {Marker, Popup} from 'react-map-gl';
+import Markers from './Markers/Markers';
+import ReactMapGl, { Popup} from 'react-map-gl';
 import axios from 'axios';
 import styles from './Map.module.css';
 
@@ -18,7 +19,6 @@ export default function Map() {
   const [loading, setLoading] = useState(true);
   const [policeData, setPoliceData] = useState([])
   const [selectedCrime, setSelectedCrime] = useState(null)
-
 
   useEffect(() => {
     //get the current users location with permision
@@ -42,57 +42,29 @@ export default function Map() {
    <> 
     <h1 className={styles.map_heading}>Our Map</h1>
     <div className={styles.map}>
-        <ReactMapGl
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onViewportChange={(viewport => setViewport(viewport))}
-        mapStyle="mapbox://styles/sjohnston00/ckf3u6kt90x0j1and9dg4n7cf?optimze=true"
-        onClick={() => setSelectedCrime(null)}
-        >
-          {policeData.map((crime) => (
-            <Marker
-              key={crime.id}
-              latitude={Number(crime.location.latitude)}
-              longitude={Number(crime.location.longitude)}
-            >
-              <button
-              className={styles.icon_button}
-              onClick={e => {
-                e.preventDefault()
-                setSelectedCrime(crime);
-              }}>
-                <svg className={styles.icon} viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/>
-                </svg>
+      <ReactMapGl
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={(viewport => setViewport(viewport))}
+      mapStyle="mapbox://styles/sjohnston00/ckfzik7uk19n419nyty2pubtj?optimze=true"
+      onClick={() => setSelectedCrime(null)}
+      >
+        <Markers 
+          policeData={policeData} 
+          setSelectedCrime={setSelectedCrime} 
+          selectedCrime={selectedCrime}
+        />
 
-              </button>
-            </Marker>
-          ))}
+        <MapButton/>
 
-          {selectedCrime && (
-            <Popup
-              latitude={Number(selectedCrime.location.latitude)}
-              longitude={Number(selectedCrime.location.longitude)}
-              className={styles.popup}
-              onClose={() => setSelectedCrime(null)}
-            >
-              <div>
-                <h2>{selectedCrime.category}</h2>
-                <p>Location: {selectedCrime.location.street.name}</p>
-                {selectedCrime.outcome_status && <p>Outcome: {selectedCrime.outcome_status.category}</p>}
-                <i>Date: {selectedCrime.month}</i>
-              </div>
-            </Popup>
-          )}
-          <MapButton/>
-          <Sidebar 
-            loading={loading} 
-            setLoading={setLoading} 
-            policeData={policeData} 
-            setPoliceData={setPoliceData} 
-            viewport={viewport}
-            setViewport={setViewport}
-          />
+        <Sidebar 
+          loading={loading} 
+          setLoading={setLoading} 
+          policeData={policeData} 
+          setPoliceData={setPoliceData} 
+          viewport={viewport}
+          setViewport={setViewport}
+        />
       </ReactMapGl>
       </div>  
     </>
---
+++ src/components/Map/Markers/Markers.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {Marker, Popup} from 'react-map-gl';
+import styles from "./Markers.module.css";
+
+const Markers = React.memo(function Components({policeData, setSelectedCrime, selectedCrime}) {
+  return (
+    <>
+    {policeData.map((crime) => (
+      <Marker
+        key={crime.id}
+        latitude={Number(crime.location.latitude)}
+        longitude={Number(crime.location.longitude)}
+      >
+        <button
+        className={styles.icon_button}
+        onClick={e => {
+          e.preventDefault()
+          setSelectedCrime(crime);
+        }}>
+          <svg className={styles.icon} viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/>
+          </svg>
+
+        </button>
+      </Marker>
+    ))}
+
+    {selectedCrime && (
+      <Popup
+        latitude={Number(selectedCrime.location.latitude)}
+        longitude={Number(selectedCrime.location.longitude)}
+        className={styles.popup}
+        onClose={() => setSelectedCrime(null)}
+      >
+        <div>
+          <h2>{selectedCrime.category}</h2>
+          <p>Location: {selectedCrime.location.street.name}</p>
+          {selectedCrime.outcome_status && <p>Outcome: {selectedCrime.outcome_status.category}</p>}
+          <i>Date: {selectedCrime.month}</i>
+        </div>
+      </Popup>
+    )}
+  </>
+  )
+});
+
+export default Markers
