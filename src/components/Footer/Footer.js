e8cd4631cb83d44f2b3ff460e3ff9c4055e6a237
Sam Johnston <johnstonsam712@gmail.com>
2020-11-05 13:15:38 +0000

    Adding my name at thee footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <div className={styles.bgBlue}>
-      
+      <p>© Sam Johnston, 2020</p>
     </div>
   )
 }
