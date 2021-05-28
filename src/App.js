fa6def6efbc07511f749f49a7fa9066e9911116d
Sam Jonston <johnstonsam712@gmail.com>
2021-05-28 13:21:53 +0100

    Initial startup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Map from './components/Map/Map'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import React from "react";
+import Map from "./components/Map/Map";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Info from "./components/Middle Section/Info/Info";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Info/>
-    <Map></Map>
-    <Footer/>
+      <Header />
+      <Info />
+      <Map></Map>
+      <Footer />
     </>
   );
 }
